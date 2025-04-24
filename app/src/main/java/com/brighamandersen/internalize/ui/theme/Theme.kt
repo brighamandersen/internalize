c4894d6f483028a081829c049d2cdf42bb8becd0
Brigham Andersen <brighambandersen@gmail.com>
2025-04-23 23:15:10 -0600

    fix status bar and bottom bar colors

--- app/src/main/java/com/brighamandersen/internalize/ui/theme/Theme.kt
+++ app/src/main/java/com/brighamandersen/internalize/ui/theme/Theme.kt
@@ -5,6 +5,12 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import android.app.Activity
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColors(
     primary = PureBlack,
@@ -35,8 +41,26 @@ fun Theme(
 ) {
     val colors = if (darkTheme) DarkColorScheme else LightColorScheme
 
+    StatusBarColor(color = colors.background)
+
     MaterialTheme(
         colors = colors,
         content = content
     )
+}
+
+@Composable
+fun StatusBarColor(color: Color) {
+    val view = LocalView.current
+    val darkTheme = isSystemInDarkTheme()
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = color.toArgb()
+            window.navigationBarColor = color.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !darkTheme
+        }
+    }
 }
