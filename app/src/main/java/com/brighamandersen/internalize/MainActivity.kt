278efcf829a64a7f57263d8941fa51004563b79b
Brigham Andersen <brighambandersen@gmail.com>
2025-08-30 17:07:53 -0600

    Clean up database singleton

--- app/src/main/java/com/brighamandersen/internalize/MainActivity.kt
+++ app/src/main/java/com/brighamandersen/internalize/MainActivity.kt
@@ -3,7 +3,6 @@ package com.brighamandersen.internalize
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.room.Room
 import com.brighamandersen.internalize.data.PassageDatabase
 import com.brighamandersen.internalize.ui.theme.Theme
 import com.brighamandersen.internalize.viewmodels.PassageViewModel
@@ -12,12 +11,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        val db = Room.databaseBuilder(
-            applicationContext,
-            PassageDatabase::class.java,
-            "passages_db"
-        ).build()
-        
+        val db = PassageDatabase.getDatabase(applicationContext)
         val passageViewModel = PassageViewModel(db.passageDao())
         
         setContent {
--- app/src/main/java/com/brighamandersen/internalize/data/AppDatabase.kt
+++ app/src/main/java/com/brighamandersen/internalize/data/AppDatabase.kt
@@ -1,10 +1,29 @@
 package com.brighamandersen.internalize.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.brighamandersen.internalize.entities.Passage
 
 @Database(entities = [Passage::class], version = 1, exportSchema = false)
 abstract class PassageDatabase : RoomDatabase() {
     abstract fun passageDao(): PassageDao
+    
+    companion object {
+        @Volatile
+        private var INSTANCE: PassageDatabase? = null
+        
+        fun getDatabase(context: Context): PassageDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    PassageDatabase::class.java,
+                    "internalize_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 } 
